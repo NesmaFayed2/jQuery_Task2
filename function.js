4c600f2ecba84a1b96877d8ccbc6406b698dd8a7
nesma fayed <nesma.fayed2002@gmail.com>
2025-04-08 17:43:00 +0200

    adding fade

--- function.js
+++ function.js
@@ -1,15 +1,31 @@
 function showImage(index) {
   $("#slider-image").attr("src", imageSources[index]);
+  $(".slider-container").fadeIn(500);
+}
+function changeImage() {
+  $("#slider-image")
+    .fadeOut(500, function () {
+      $(this).attr("src", imageSources[currentIndex]);
+    })
+    .fadeIn(500);
 }
 
 function nextImage() {
   currentIndex = (currentIndex + 1) % imageSources.length;
-  showImage(currentIndex);
+  changeImage();
 }
 
 function prevImage() {
   currentIndex = (currentIndex - 1 + imageSources.length) % imageSources.length;
-  showImage(currentIndex);
+  changeImage();
+}
+
+function hideSliderOnEscape() {
+  $(document).on("keydown", function (e) {
+    if (e.key === "Escape") {
+      $(".slider-container").fadeOut(300);
+    }
+  });
 }
 
 window.nextImage = nextImage;
--- task.js
+++ task.js
@@ -2,17 +2,20 @@ var currentIndex = 0;
 var imageSources = [];
 
 $(document).ready(function () {
-  ////////
+  //__________________
   $(".images").each(function () {
     imageSources.push($(this).attr("src"));
   });
-  ////////
+  //__________________
+
   $(".images").on("click", function () {
     currentIndex = $(".images").index(this);
-    $(".slider-container").show();
     showImage(currentIndex);
   });
-
+  //____________________
   $("#prev").on("click", prevImage);
   $("#next").on("click", nextImage);
+
+  // __________________
+  hideSliderOnEscape();
 });
